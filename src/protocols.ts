bccdb82005a45b5a072bd528bc5432ff55e05591
JeanPiton <jean.orpiton@gmail.com>
2023-12-03 13:40:05 -0300

    feat: menu item component

--- src/protocols.ts
+++ src/protocols.ts
@@ -3,3 +3,26 @@ type Type = {
     name: string,
     image: string
 }
+
+type MenuItemData = {
+    id: number;
+    name: string;
+    desc: string;
+    sdesc: string;
+    price: number;
+    image: string;
+    typeId: number;
+    selled: number;
+    extra: {
+        id: number;
+        name: string;
+        image: string;
+        price: number;
+        desc: string;
+    }[];
+    type: {
+        id: number;
+        name: string;
+        image: string;
+    };
+}
